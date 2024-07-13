8320925cf41527613ac7a1bc82d79cc5c7275afb
Rafeeq Shaik <rafeeq.shaik@valuelabs.com>
2024-07-13 13:05:02 +0530

    Numbers bigger than 1000 should be ignoring

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,9 +1,17 @@
 class StringCalculator
 	def self.add(str)
 		return 0 if str.empty?
+		
 		delimiter = str.start_with?("//") ? str[2] : ","
 		str = str.split("\n", 2).last if str.start_with?("//")
+		
 		numbers = str.split(/[\n#{delimiter}]/).map(&:to_i)
+		
+		negative_numbers = numbers.select { |n| n < 0 }
+		raise ArgumentError, "negative numbers not allowed: #{negative_numbers.join(', ')}" unless negative_numbers.empty?
+
+		numbers.select! { |n| n <= 1000 }  # Ignoring the numbers greater than 1000
+
 		numbers.sum
 	end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -25,4 +25,20 @@ RSpec.describe 'add_string' do
   it 'adding two numbers with custom delimiter' do
     expect(StringCalculator.add("//\n\n1\n2")).to eq(3)
   end
+
+  it 'raises an error with negative numbers' do
+    expect { StringCalculator.add("1,2,-3") }.to raise_error(ArgumentError, "negative numbers not allowed: -3")
+  end
+
+  it 'raises an error with negative numbers' do
+    expect { StringCalculator.add("1,-2,-3") }.to raise_error(ArgumentError, "negative numbers not allowed: -2, -3")
+  end
+
+  it 'ignoring numbers bigger than 1000' do
+    expect(StringCalculator.add("2,1000")).to eq(1002)
+  end
+
+  it 'ignoring numbers bigger than 1000' do
+    expect(StringCalculator.add("2,1001")).to eq(2)
+  end
 end
